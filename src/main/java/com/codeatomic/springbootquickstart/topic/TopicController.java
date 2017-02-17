d9dcc7e6e01308499722427741437b9458f68a94
Jewel Ahmed <tojibon@gmail.com>
2017-02-17 21:19:25 +0600

    Getting a single resource. Ex: /topics/spring

--- src/main/java/com/codeatomic/springbootquickstart/topic/TopicController.java
+++ src/main/java/com/codeatomic/springbootquickstart/topic/TopicController.java
@@ -3,6 +3,7 @@ package com.codeatomic.springbootquickstart.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,10 @@ public class TopicController {
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
 	}
 }
--- src/main/java/com/codeatomic/springbootquickstart/topic/TopicService.java
+++ src/main/java/com/codeatomic/springbootquickstart/topic/TopicService.java
@@ -17,4 +17,8 @@ public class TopicService {
 	public List<Topic> getAllTopics() {
 		return this.topics;
 	}
+
+	public Topic getTopic(String id) {
+		return (Topic) this.topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
 }
