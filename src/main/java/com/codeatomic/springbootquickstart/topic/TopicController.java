88cb875554316e0230249bdd2fb07081e25ff2b7
Jewel Ahmed <tojibon@gmail.com>
2017-02-17 21:34:43 +0600

    Added RequestMapping instead of PostMapping to /topics Just to be sure

--- src/main/java/com/codeatomic/springbootquickstart/topic/TopicController.java
+++ src/main/java/com/codeatomic/springbootquickstart/topic/TopicController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,7 +26,8 @@ public class TopicController {
 		return topicService.getTopic(id);
 	}
 	
-	@PostMapping("/topics")
+	//@PostMapping("/topics")
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
 	public String addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 		String response = "{\"success\": true, \"message\": Topic has been added successfully.}";
